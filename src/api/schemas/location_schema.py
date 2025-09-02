d8beb38341631728cd8f4527821fde35d99688de
Kaynguyen6024 <kaynguyen2405@gmail.com>
2025-09-02 22:27:38 +0700

    sủa location

--- src/api/schemas/location_schema.py
+++ src/api/schemas/location_schema.py
@@ -1,6 +1,14 @@
 from marshmallow import Schema, fields
 
-class LocationSchema(Schema):
-    id = fields.Int()
-    name = fields.Str()
+class LocationRequestSchema(Schema):
+    name = fields.Str(required=True)
+    address = fields.Str(required=False)
+    description = fields.Str(required=False)
+
+class LocationResponseSchema(Schema):
+    id = fields.Int(required=True)
+    name = fields.Str(required=True)
     address = fields.Str()
+    description = fields.Str()
+    created_at = fields.Raw()
+    updated_at = fields.Raw()
